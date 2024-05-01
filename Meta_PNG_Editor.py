99177dac64f8f829342cc88804e867ef237d6e61
petrvavrin <148798764+petrvavrin@users.noreply.github.com>
2024-05-01 21:30:54 +0200

    Add files via upload

--- Meta_PNG_Editor.py
+++ Meta_PNG_Editor.py
@@ -17,40 +17,46 @@ counter = 1
 input_dir = os.path.dirname(os.path.abspath(__file__))
 
 choice = input(
-    "------------------------------------------\n"
-    "Which type of function do you want to use?\n"
-    "1. Remove metadata from image\n"
-    "2. to be added\n"
-    "3. Exit\n"
-    "------------------------------------------\n"
+  "------------------------------------------\n"
+  "Which type of function do you want to use?\n"
+  "1. Remove metadata from image\n"
+  "2. to be added\n"
+  "3. Exit\n"
+  "------------------------------------------\n"
 )
 
 
-if choice in ["1", "2", "3"]:
-  pass
-else:
-  print('Select valid number and try again')
-  exit(0)
+def remove_metadata():
+  for filename in os.listdir(input_dir):
+    # Check if the file is a PNG file
+    if filename.endswith(".png"):
+        # Open the image file
+        image_path = os.path.join(input_dir, filename)
+        image = Image.open(image_path)
+        new_filename = f"pic{counter}.png"
+        image.save(new_filename)
+        image.close()
+        counter += 1
 
 
-if choice == '1':
-    for filename in os.listdir(input_dir):
-            # Check if the file is a PNG file
-            if filename.endswith(".png"):
-                # Open the image file
-                image_path = os.path.join(input_dir, filename)
-                image = Image.open(image_path)
-                new_filename = f"pic{counter}.png"
-                image.save(new_filename)
-                image.close()
-                counter += 1
+def print_menu():
+  if choice in ["1", "2", "3"]:
+    pass
+  else:
+    print('Select valid number and try again')
+    exit(0)
+
+
+  if choice == '1':
+    remove_metadata()
     print('Done')
 
-
-elif choice == '2':
-
+  elif choice == '2':
+    exit(0)
     print('Done')
 
-elif choice == '3':
-  print('Exiting ...')
-  exit(0)
+  elif choice == '3':
+    print('Exiting ...')
+    exit(0)
+
+print_menu()
