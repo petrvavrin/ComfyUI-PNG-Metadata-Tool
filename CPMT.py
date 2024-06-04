9b049a692fab8ad434f92e85f196741a758c5146
petrvavrin <148798764+petrvavrin@users.noreply.github.com>
2024-06-04 20:33:19 +0200

    Add files via upload

--- CPMT.py
+++ CPMT.py
@@ -18,17 +18,18 @@ from progress.bar import Bar
 import os
 import glob
 import hashlib
+import shutil
 
 
 
 choice = input(
-  "------------------------------------------\n"
-  "Which type of function do you want to use?\n"
-  "1. Remove metadata and create MD5 checksums\n"
-  "2. Remove metadata from images\n"
-  "3. Read metadata from image\n"
-  "4. Exit\n"
-  "------------------------------------------\n"
+  '------------------------------------------\n'
+  'Which type of function do you want to use?\n'
+  '1. Remove metadata and create MD5 checksums\n'
+  '2. Remove metadata from images\n'
+  '3. Read metadata from image\n'
+  '4. Exit\n'
+  '------------------------------------------\n'
 )
 
 
@@ -41,7 +42,7 @@ def remove_metadata_and_hash():
   path1 = os.path.join(parent_dir, dir_name)
 
   try:
-    pngCounter = len(glob.glob1(source_dir,"*.png"))
+    pngCounter = len(glob.glob1(source_dir,'*.png'))
     bar = Bar('Removing metadata...', max=pngCounter, suffix = '%(percent).1f%% - ETA: %(eta)ds')
 
     os.mkdir(path1)
@@ -51,7 +52,7 @@ def remove_metadata_and_hash():
     for filename in os.listdir(source_dir):
 
       # Check if the file is a PNG file
-      if filename.endswith(".png"):
+      if filename.endswith('.png'):
         # Open the image file
         image_path = os.path.join(source_dir, filename)
         #Load image
@@ -64,39 +65,45 @@ def remove_metadata_and_hash():
     bar.finish()
     print('Removing Done!')
 
+    bat = open(os.path.join(path1, 'Verify files.bat'), 'w')
+    bat.write(
+    '@echo off\n'
+    'cd /d "%~dp0"\n'
+    'cd MD5\n'
+    'start QuickSFV.EXE data-checksum.md5\n'
+    )
+    bat.close()
+    print('BAT created!')
 
     path3 = os.path.join(path1, 'MD5')
-    os.mkdir(path3)
-    
+    shutil.copytree(os.path.join(os.getcwd(), 'QuickSFV'), path3)
     print('MD5 folder created!')
+    print('QuickSFV files copied')
 
-
-    pngCounter = len(glob.glob1(source_dir,"*.png"))
+    pngCounter = len(glob.glob1(source_dir,'*.png'))
     bar = Bar('Creating MD5 checksums...', max=pngCounter, suffix = '%(percent).1f%% - ETA: %(eta)ds')
 
     output_path = os.path.join(path3, 'data-checksum.md5')
-    checksum_file = open(output_path, "w")
+    checksum_file = open(output_path, 'w')
 
     for filename in os.listdir(path2):
 
       # Check if the file is a PNG file
-      if filename.endswith(".png"):
+      if filename.endswith('.png'):
         # Open the image file
         image_path = os.path.join(path2, filename)
         #Load image
-        img = open(image_path, "rb")
+        img = open(image_path, 'rb')
         data = img.read()
         md5_returned = hashlib.md5(data).hexdigest()
 
 
         checksum_file.write(f'{md5_returned} *..\\ComfyUI_ALL_no_meta\\{filename}\n')
 
-
         bar.next()
 
     bar.finish()
     print('MD5 checksums done!')
-
 
 
   except:
@@ -112,7 +119,7 @@ def remove_metadata():
   path = os.path.join(parent_dir, dir_name)
 
   try:
-    pngCounter = len(glob.glob1(source_dir,"*.png"))
+    pngCounter = len(glob.glob1(source_dir,'*.png'))
     bar = Bar('Processing...', max=pngCounter, suffix = '%(percent).1f%% - ETA: %(eta)ds')
 
     os.mkdir(path)
@@ -120,7 +127,7 @@ def remove_metadata():
     for filename in os.listdir(source_dir):
 
       # Check if the file is a PNG file
-      if filename.endswith(".png"):
+      if filename.endswith('.png'):
         # Open the image file
         image_path = os.path.join(source_dir, filename)
         #Load image
@@ -145,12 +152,12 @@ def read_metadata():
     image.close()
     print('Done')
   except:
-    print("Wrong path!")
+    print('Wrong path!')
     exit(0)
 
 
 def print_menu():
-  if choice in ["1", "2", "3", "4"]:
+  if choice in ['1', '2', '3', '4']:
     pass
   else:
     print('Select valid number and try again')
